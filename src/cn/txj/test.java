8d4b6b19b4675476586e7e30274395ff059e38da
tanxiaojiang <352457723@qq.com>
2018-07-21 10:37:31 +0800

    txj  分支提交的东西

--- src/cn/txj/test.java
+++ src/cn/txj/test.java
@@ -5,10 +5,11 @@ public class test {
 
    public  void  say1(){
 	   System.out.println("我是brh的say1（）方法");
+	   String f="dasda";
    }
 	
 	   public  void  say3(){
 	   System.out.println("我是brh的say3（）方法");
 		   System.out.println("我是brh的say3vvvvvv（）方法");
-   }
+       }
 }
