449272297b99afeb87debde713e740306d6157f2
tanxiaojiang <31946046+tanxiaojiang@users.noreply.github.com>
2018-07-21 10:33:39 +0800

    Update test.java

--- src/cn/txj/test.java
+++ src/cn/txj/test.java
@@ -6,7 +6,8 @@ public class test {
    public  void  say1(){
 	   System.out.println("我是brh的say1（）方法");
    }
-   public  void  say2(){
-	   System.out.println("我是brh的say2（）方法");
+	
+	   public  void  say3(){
+	   System.out.println("我是brh的say3（）方法");
    }
 }
